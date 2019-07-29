e87b7f2573942e2fd1428be06a9f9238a37be70e
yusufcengel <y.cengel@hotmail.com>
2019-07-29 09:49:44 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace email_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_send = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Body = new System.Windows.Forms.TextBox();
+            this.txt_Subject = new System.Windows.Forms.TextBox();
+            this.txt_BCC = new System.Windows.Forms.TextBox();
+            this.txt_CS = new System.Windows.Forms.TextBox();
+            this.txt_TO = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(352, 313);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 14;
+            this.btn_send.Text = "gönder";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(20, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Body";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "bcc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Subject";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "cs:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "to:";
+            // 
+            // txt_Body
+            // 
+            this.txt_Body.Location = new System.Drawing.Point(73, 123);
+            this.txt_Body.Multiline = true;
+            this.txt_Body.Name = "txt_Body";
+            this.txt_Body.Size = new System.Drawing.Size(354, 173);
+            this.txt_Body.TabIndex = 8;
+            // 
+            // txt_Subject
+            // 
+            this.txt_Subject.Location = new System.Drawing.Point(73, 97);
+            this.txt_Subject.Name = "txt_Subject";
+            this.txt_Subject.Size = new System.Drawing.Size(354, 20);
+            this.txt_Subject.TabIndex = 4;
+            // 
+            // txt_BCC
+            // 
+            this.txt_BCC.Location = new System.Drawing.Point(73, 71);
+            this.txt_BCC.Name = "txt_BCC";
+            this.txt_BCC.Size = new System.Drawing.Size(354, 20);
+            this.txt_BCC.TabIndex = 5;
+            // 
+            // txt_CS
+            // 
+            this.txt_CS.Location = new System.Drawing.Point(73, 45);
+            this.txt_CS.Name = "txt_CS";
+            this.txt_CS.Size = new System.Drawing.Size(354, 20);
+            this.txt_CS.TabIndex = 6;
+            // 
+            // txt_TO
+            // 
+            this.txt_TO.Location = new System.Drawing.Point(73, 19);
+            this.txt_TO.Name = "txt_TO";
+            this.txt_TO.Size = new System.Drawing.Size(354, 20);
+            this.txt_TO.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Body);
+            this.Controls.Add(this.txt_Subject);
+            this.Controls.Add(this.txt_BCC);
+            this.Controls.Add(this.txt_CS);
+            this.Controls.Add(this.txt_TO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Body;
+        private System.Windows.Forms.TextBox txt_Subject;
+        private System.Windows.Forms.TextBox txt_BCC;
+        private System.Windows.Forms.TextBox txt_CS;
+        private System.Windows.Forms.TextBox txt_TO;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+using System.Windows.Forms;
+
+namespace email_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Gonder(txt_Subject.Text, txt_Body.Text, txt_TO.Text.ToLower().Split(';'), txt_CS.Text.ToLower().Split(';'), txt_BCC.Text.ToLower().Split(';'));
+        }
+        public bool Gonder(string konu, string icerik, string[] ToGonderilen, string[] CcGönderilen, string[] BccGonderilen)
+        {
+            MailMessage ePosta = new MailMessage();
+            ePosta.From = new MailAddress("sburaksg@outlook.com");
+            
+
+            for (int i = 0; i < ToGonderilen.Length; i++)
+            {
+                ePosta.To.Add(ToGonderilen[i]);
+            }
+            
+            for (int i = 0; i < CcGönderilen.Length; i++)
+            {
+                ePosta.CC.Add(CcGönderilen[i]);
+            }
+            for (int i = 0; i < BccGonderilen.Length; i++)
+            {
+                ePosta.Bcc.Add(BccGonderilen[i]);
+            }
+            ePosta.Subject = konu;
+            
+            ePosta.Body = icerik;
+            
+            SmtpClient smtp = new SmtpClient();
+           
+            smtp.Credentials = new System.Net.NetworkCredential("odev.gonder.39@hotmail.com", "123*ysfF");
+            smtp.Port = 587;
+            smtp.Host = "smtp-mail.outlook.com";
+            smtp.EnableSsl = true;
+            object userState = ePosta;
+            bool kontrol = true;
+            try
+            {
+                smtp.SendAsync(ePosta, (object)ePosta);
+            }
+            catch (SmtpException ex)
+            {
+                kontrol = false;
+                System.Windows.Forms.MessageBox.Show(ex.Message, "Mail Gönderme Hatasi");
+            }
+            return kontrol;
+
+
+        }
+    }
+}
